02e91a0b6770e4464323b63356b63c56c28afb60
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-10-18 16:14:46 +0600

    https://github.com/MamajonovMuhriddin/1.1.3.git

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("John", "Doe", (byte) 25);
+        userService.saveUser("Jane", "Doe", (byte) 30);
+        userService.saveUser("Mike", "Smith", (byte) 35);
+        userService.saveUser("Emily", "Johnson", (byte) 40);
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,8 +1,10 @@
 package jm.task.core.jdbc.model;
 
+
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 
 @Table
 public class User {
@@ -22,10 +24,12 @@ public class User {
 
     }
 
-    public User(String name, String lastName, Byte age) {
+    public User( String name, String lastName, Byte age) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
+
+
     }
 
     public Long getId() {
@@ -58,5 +62,15 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
